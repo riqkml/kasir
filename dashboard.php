52098a31b928a2aa50e15c490c587a4e02298880
riqkml <riqkikamal@outlook.com>
2020-06-15 17:08:35 +0700

    master commit sidebar feature

---
+++ dashboard.php
@@ -0,0 +1,354 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Responsive sidebar template with sliding effect and dropdown menu based on bootstrap 3">
+    <title>Sidebar template</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/dashboard.css">
+ 
+
+</head>
+
+<body>
+
+<div class="page-wrapper chiller-theme toggled">
+ 
+  <nav id="sidebar" class="sidebar-wrapper sidenav">
+    <div class="sidebar-content">
+      
+      <div class="sidebar-header">
+        <div class="user-pic">
+        <img src="img/user.png" class="rounded-circle" alt="Cinque Terre"> 
+        </div>
+        <div class="user-info">
+          <span class="user-name">Jhon
+            <strong>Smith</strong>
+          </span>
+          <span class="user-role">Administrator</span>
+          <span class="user-status">
+            <i class="fa fa-circle"></i>
+            <span>Online</span>
+          </span>
+        </div>
+      </div>
+      <!-- sidebar-header  -->
+      <div class="sidebar-search">
+        <div>
+          <div class="input-group">
+            <input type="text" class="form-control search-menu" placeholder="Search...">
+            <div class="input-group-append">
+              <span class="input-group-text">
+                <i class="fa fa-search" aria-hidden="true"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- sidebar-search  -->
+      <div class="sidebar-menu">
+        <ul>
+          <li class="header-menu">
+            <span>General</span>
+          </li>
+          <li class="sidebar">
+            <a href="#">
+              <i class="fa fa-tachometer-alt"></i>
+              <span>Dashboard</span>
+              <span class="badge badge-pill badge-warning">New</span>
+            </a>
+          </li>
+          <li class="sidebar">
+            <a href="#">
+              <i class="fa fa-users"></i>
+              <span>Akun</span>
+              <span class="badge badge-pill badge-success">New</span>
+            </a>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-shopping-cart"></i>
+              <span>Menu</span>
+              <span class="badge badge-pill badge-danger">3</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Makanan Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Obat Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Aksesoris Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Food Plate Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Acc Liter Box Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Cage Kucing</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="far fa-gem"></i>
+              <span>Layanan</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Smoothing Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Penitipan Kucing</a>
+                </li>
+                <li>
+                  <a href="#">Vaksin Kucing</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-chart-line"></i>
+              <span>Laporan</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Laporan Transaksi</a>
+                </li>
+                <li>
+                  <a href="#">Laporan Menu Penjualan</a>
+                </li>
+                <li>
+                  <a href="#">Laporan Keuntungan</a>
+                </li>
+                <li>
+                  <a href="#">Laporan Stok Barang</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fa fa-cogs"></i>
+              <span>Pengaturan</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li>
+                  <a href="#">Edit Profil</a>
+                </li>
+                <li>
+                  <a href="#">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+         
+        </ul>
+      </div>
+      <!-- sidebar-menu  -->
+    </div>
+    <div class="sidebar-footer">
+    <small>Optimized by Amrel</small>
+    </div>
+    <!-- sidebar-content  -->
+  </nav>
+  <!-- sidebar-wrapper  -->
+  <main class="page-content">
+    <div class="container container-bottom">
+        <div class="row">
+            <div class="col-10">
+                <h2>Teebee Petshop</h2>
+                <small><i>Dasboard/main</i></small>
+            </div>   
+            <div class="col-2">
+                
+            </div> 
+        </div>
+    </div>
+    <div class="container">
+    <div class="row">
+			 
+             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                     <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                     
+                     <div class="title">
+                         <h4>Instagram</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                     </div>
+                     
+                     <a href="#">Learn More</a>
+                     
+                  </div>
+             </div>	 
+             
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                     <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                 
+                     <div class="title">
+                         <h4>Twitter</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                     </div>
+                     
+                     <a href="#">Learn More</a>
+                     
+                  </div>
+             </div>	 
+             
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                     <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                     
+                     <div class="title">
+                         <h4>Facebook</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                     </div>
+                     
+                     <a href="#">Learn More</a>
+                     
+                  </div>
+             </div>	 
+             
+             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                     <i class="fa fa-pinterest-p fa-3x" aria-hidden="true"></i>
+                     
+                     <div class="title">
+                         <h4>Pinterest</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                     </div>
+                     
+                     <a href="#">Learn More</a>
+                     
+                  </div>
+             </div>	 
+             
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                     <i class="fa fa-google-plus fa-3x" aria-hidden="true"></i>
+                 
+                     <div class="title">
+                         <h4>Google</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                     </div>
+                     
+                     <a href="#">Learn More</a>
+                     
+                  </div>
+             </div>	 
+             
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+                 <div class="box-part text-center">
+                     
+                     <i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                     
+                     <div class="title">
+                         <h4>Github</h4>
+                     </div>
+                     
+                     <div class="text">
+                         <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+                     </div>
+                     
+                     <a href="#">Learn More</a>
+                     
+                  </div>
+             </div>
+     
+     </div>		
+    </div>
+  </main>
+  <!-- page-content" -->
+</div>
+<!-- page-wrapper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+    <script>
+    jQuery(function ($) {
+
+$(".sidebar-dropdown > a").click(function() {
+$(".sidebar-submenu").slideUp(200);
+if (
+$(this)
+  .parent()
+  .hasClass("active")
+) {
+$(".sidebar-dropdown").removeClass("active");
+$(this)
+  .parent()
+  .removeClass("active");
+} else {
+$(".sidebar-dropdown").removeClass("active");
+$(this)
+  .next(".sidebar-submenu")
+  .slideDown(200);
+$(this)
+  .parent()
+  .addClass("active");
+}
+});
+
+$("#close-sidebar").click(function() {
+$(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+$(".page-wrapper").addClass("toggled");
+});
+
+
+
+
+});
+</script>
+</body>
+
+</html>
